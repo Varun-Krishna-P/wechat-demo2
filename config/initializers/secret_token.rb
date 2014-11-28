e032bd07189858f2c5228ba302d52a075c6461f0
WebCrafters1 <machine1@webcrafters.in>
2014-11-28 11:17:59 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WechatDemo2::Application.config.secret_token = 'cf721155db4644ccd2faad78700c42732de08a7e679e7a586ab8e71d2059c6f422c90c4cac0c608fa7a7f8e4a8abafb34ab090e986598f46d69a9e5d281d46a9'
